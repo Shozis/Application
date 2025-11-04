db30c4a961b66a6ec16df83b450eca8fe651acf1
Shozis <144358159+Shozis@users.noreply.github.com>
2025-11-05 00:43:53 +0300

    clicker done

--- fullstack-frontend/app/Clicker.tsx
+++ fullstack-frontend/app/Clicker.tsx
@@ -1,20 +1,42 @@
-// import { useState } from "react";
-// import { View, Text, Button } from "react-native";
+import { useState } from "react";
+import { View, Text, Button } from "react-native";
 
 
 
-// const Clicker = () => {
+const Clicker = () => {
 
-//     const [data, setData] = useState(Number);
+    const [data, setData] = useState(0);
 
-//     return(
-//     <View>
-//         <Button title="click me" onPress={() => }/>
+    const txt = "that text will done if you will click more times";
 
-//         <Text>
+    const dotsCount = data > txt.length ? data - txt.length: 0;
+    const breakTxt = txt.substring(0, data) + (".".repeat(dotsCount));
 
-//         </Text>
-//     </View>
-// )}
 
-// export default Clicker;
+    return (
+        <View>
+            <Button title="click me" onPress={() => setData(data + 1)} />
+            <Text>
+                нажато: {data} раз
+            </Text>
+                        <View style={{
+                    flexDirection: "row",
+                }}>
+                    {breakTxt.split(' ').map((word,index) => (
+                        <Text 
+                            key={index}
+                            style={{
+                                marginRight: 8,
+                            }}
+                        >
+                            {word}
+                        </Text>
+                        
+                    ))}
+                </View>
+
+        </View>
+    )
+}
+
+export default Clicker;
