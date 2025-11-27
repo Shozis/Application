17da29ff5abd6c95a1f2380959fc7c66da5b3842
Shozis <nik.sosnin.07@inbox.ru>
2025-11-27 12:54:57 +0300

    get (id)

--- fullstack-backend/src/api/_dataCourse.js
+++ fullstack-backend/src/api/_dataCourse.js
@@ -1,16 +1,25 @@
-import {createRequire} from "module";
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 const data = require('../../data/data.json')
 
-export const  dataCourse = (app) => {
+export const dataCourse = (app) => {
     app.get('/api/data', (req, res) => {
-        console.log('start request') 
+        console.log('start request')
         return res.json(data.dataD);
     });
 
     app.get('/api/data/:id', (req, res) => {
         const idReq = req.params.id;
-        console.log('start request id') 
+        console.log('start request id' + idReq);
+
+
+
+        for (let i = 0; data.dataD.length > i; i++) {
+            if (data.dataD[i].id === idReq) {
+                return res.json(data.dataD[i]);
+            }
+            return res.status(404).send("Data not found");
+        }
     })
 }
