12371d5425d3e53c46e5fe8043f4380892760b9e
Shozis <nik.sosnin.07@inbox.ru>
2025-12-11 11:55:04 +0300

    ///

--- fullstack-backend/src/api/_dataCourse.js
+++ fullstack-backend/src/api/_dataCourse.js
@@ -1,4 +1,5 @@
 import { createRequire } from "module";
+import { updateJsonFile } from "../helpers/_update";
 const require = createRequire(import.meta.url);
 
 const data = require('../../data/data.json')
@@ -24,5 +25,30 @@ export const dataCourse = (app) => {
         }
 
         return res.status(404).send("Data not found");
+    });
+
+    app.post('/api/course/', (req, res) => {
+        console.log('create new element');
+        
+        let newId = 0;
+
+        if (data.dataD.length !== 0) {
+            newId = data.dataD[data.dataD.length - 1].id + 1;
+        }
+
+        console.log(newId);
+        const createdData = req.body;
+
+        data.dataD.push({
+            id: newId, 
+            name: createdData,
+            age: createdData.age
+        });
+
+        updateJsonFile('data.json', data);
+
+        console.log('created complited')
+
+        return res.json(data.dataD[data.dataD.length - 1]);
     })
 }
